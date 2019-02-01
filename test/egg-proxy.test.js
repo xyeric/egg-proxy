2cf914dbe738a7b70f98fb87091ae303de0e60a8
zhenghongfei <zhenghongfei@cmos.chinamobile.com>
2019-02-01 13:56:06 +0800

    feat: typescript support

--- test/egg-proxy.test.js
+++ test/egg-proxy.test.js
@@ -1,36 +1,92 @@
 'use strict';
 
-const request = require('supertest');
+const http = require('http');
 const mm = require('egg-mock');
+const request = require('supertest');
 
 describe('test/egg-proxy.test.js', () => {
-  let app;
+  let server;
   before(() => {
-    app = mm.app({
-      baseDir: 'apps/egg-proxy-test',
+    server = http.createServer((req, res) => {
+      if (req.url === '/proxy_html') {
+        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+        res.end('hi, this is proxy target');
+      } else if (req.url === '/proxy_json') {
+        res.setHeader('Content-Type', 'application/json; charset=utf-8');
+        res.end(JSON.stringify({
+          name: 'test',
+          age: 1,
+        }));
+      } else if (req.url === '/proxy_404') {
+        res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+        res.statusCode = 404;
+        res.end();
+      } else if (req.url === '/proxy_403') {
+        res.setHeader('Content-Type', 'application/json; charset=utf-8');
+        res.statusCode = 403;
+        res.end(JSON.stringify({
+          success: false,
+          message: 'no permission',
+        }));
+      }
     });
-    return app.ready();
+    server.listen(1234);
   });
 
-  after(() => app.close());
+  after(function() {
+    server.close();
+  });
+
+  let app;
   afterEach(mm.restore);
 
-  it('GET /, should return 200', () => {
-    return request(app.callback())
-      .get('/')
-      .expect('hi, proxy')
-      .expect(200);
-  });
+  [ 'egg-proxy-single', 'egg-proxy-multiple' ].forEach(name => {
+    describe(name, () => {
+      before(() => {
+        app = mm.app({
+          baseDir: `apps/${name}`,
+        });
+        return app.ready();
+      });
 
-  it('GET /test1, should return 500', () => {
-    return request(app.callback())
-      .get('/test1')
-      .expect(500);
-  });
+      after(() => app.close());
 
-  // it('GET /explore, should return 200', () => {
-  //   return request(app.callback())
-  //     .get('/explore')
-  //     .expect(200);
-  // });
+      it('GET /, should return 200', () => {
+        return request(app.callback())
+          .get('/')
+          .expect('hi, proxy')
+          .expect(200);
+      });
+
+      it('GET /proxy_html, should request html from proxy server success', () => {
+        return request(app.callback())
+          .get('/proxy_html')
+          .expect(200, 'hi, this is proxy target');
+      });
+
+      it('GET /proxy_json, should request json from proxy server success', () => {
+        return request(app.callback())
+          .get('/proxy_json')
+          .expect(200, {
+            name: 'test',
+            age: 1,
+          });
+      });
+
+      it('GET /proxy_403, should request proxy server failed with 403 error', () => {
+        return request(app.callback())
+          .get('/proxy_403')
+          .expect(403, {
+            success: false,
+            message: 'no permission',
+          });
+      });
+
+      it('GET /proxy_404, should request proxy server failed with 404 error', () => {
+        return request(app.callback())
+          .get('/proxy_404')
+          .expect(404);
+      });
+    });
+  });
 });
---
+++ test/fixtures/apps/egg-proxy-multiple/config/config.unittest.js
@@ -0,0 +1,10 @@
+'use strict';
+
+exports.keys = '123456';
+
+exports.proxy = [
+  {
+    host: 'http://127.0.0.1:1234',
+    match: /^\/proxy/,
+  },
+];
